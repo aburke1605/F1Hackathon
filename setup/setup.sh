317cabd0d309ebd16c0d74f1beb61889754ac340
aburke1605 <aodhan-burke@hotmail.co.uk>
2022-02-19 13:25:34 +0000

    changed name of env

--- setup/setup.sh
+++ setup/setup.sh
@@ -2,8 +2,8 @@ if [ ! -f "setup/miniconda.sh" ]; then
     wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O setup/miniconda.sh
 fi
 
-bash setup/miniconda.sh -b -p setup/envs &&
+bash setup/miniconda.sh -b -p setup/conda &&
 (
-    source setup/envs/bin/activate
-    conda env create -f setup/env.yaml -n base
+    source setup/conda/bin/activate
+    conda env create -f setup/env.yaml -n F1
 )
